baaf1dee4d62b4d88344cf9b68f719a40c444f2d
Megan <megan.e.dose@gmail.com>
2018-01-06 15:50:20 -0800

    Even more practice

--- quickfire_practice.py
+++ quickfire_practice.py
@@ -133,3 +133,19 @@ def scrabble_score(word):
 	return score
 
 print scrabble_score("hello")
+
+# -------------------------------------------------------------------
+# Write a function called censor that takes two strings, text and word, as input.
+# It should return the text with the word you chose replaced with asterisks.
+# For example:
+#     censor("this hack is wack hack", "hack")
+# should return:
+#     "this **** is wack ****"
+# Assume your input strings won't contain punctuation or upper case letters.
+# The number of asterisks you put should correspond to the number of letters
+# in the censored word.
+
+def censor(text, word):
+	return text.replace(word, "*" * len(word))
+
+print censor("this hack is wack hack", "hack")
