7d61bcc903204dd5be3ec1d7ea69739f423b5a87
Megan <megan.e.dose@gmail.com>
2018-01-07 20:20:15 -0800

    Another practice problem

--- quickfire_practice.py
+++ quickfire_practice.py
@@ -149,3 +149,19 @@ def censor(text, word):
 	return text.replace(word, "*" * len(word))
 
 print censor("this hack is wack hack", "hack")
+
+# -------------------------------------------------------------------
+# Define a function called count that has two arguments called sequence and item.
+# Return the number of times the item occurs in the list.
+# For example: count([1, 2, 1, 1], 1) should return 3
+# (because 1 appears 3 times in the list).
+# The item you input may be an integer, string, float, or even another list!
+
+def count(sequence, item):
+	counter = 0
+	for thing in sequence:
+		if thing == item:
+			counter += 1
+	return counter
+
+print count([1,2,1,1], 1)
